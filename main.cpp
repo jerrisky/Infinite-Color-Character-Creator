6a0a48f157f503461305cdcef0073ac7a143b452
jerrisky <2414560085@qq.com>
2024-05-12 14:15:45 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "widget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.setWindowTitle("Game");
+    w.show();
+    return a.exec();
+}
